7ae2f673222157e0c594f9cef9376033379381c0
Hugo Ángeles <hugoangeles0810@gmail.com>
2018-04-13 11:37:08 -0500

    Adding XMPPFramework

---
+++ TSMLibrary/TSMXMPP.swift
@@ -0,0 +1,27 @@
+//
+//  TSMXMPP.swift
+//  TSMLibrary
+//
+//  Created by Hugo Ángeles Chávez on 4/13/18.
+//  Copyright © 2018 Hugo Ángeles Chávez. All rights reserved.
+//
+
+import Foundation
+import XMPPFramework
+
+public class TSM2 {
+    
+    private var stream: XMPPStream!
+    
+    public init(domain: String) {
+        stream = XMPPStream()
+        stream.myJID = XMPPJID(user: "smith", domain: domain, resource: "mobile")
+    }
+    
+    public var getFullNameJID: String {
+        return stream.myJID!.full()
+    }
+    
+}
+
+
