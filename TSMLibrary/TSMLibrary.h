ea552e131512973abf0b2a8ca52e3917f04384e8
Hugo Ángeles <hugoangeles0810@gmail.com>
2018-04-13 08:41:28 -0500

    Initial Commit

---
+++ TSMLibrary/TSMLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TSMLibrary.h
+//  TSMLibrary
+//
+//  Created by Hugo Ángeles Chávez on 4/13/18.
+//  Copyright © 2018 Hugo Ángeles Chávez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSMLibrary.
+FOUNDATION_EXPORT double TSMLibraryVersionNumber;
+
+//! Project version string for TSMLibrary.
+FOUNDATION_EXPORT const unsigned char TSMLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSMLibrary/PublicHeader.h>
+
+
